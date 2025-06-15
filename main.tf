bc6ca56a7be45026b9df37fff8e6c6219040b8cf
2410781009fh <2410781009@hochschule-burgenland.at>
2025-06-15 17:16:53 +0200

    Added OpenTofu-Files

---
+++ main.tf
@@ -0,0 +1,45 @@
+resource "exoscale_sks_cluster" "my_sks_cluster" {
+  zone = var.exoscale_zone
+  name = var.exoscale_cluster_name
+  exoscale_csi = true
+  service_level = var.exoscale_sks_service_level
+}
+
+resource "exoscale_security_group" "web_sg" {
+  name = "web-access"
+}
+
+resource "exoscale_security_group_rule" "http_ingress" {
+  security_group_id = exoscale_security_group.web_sg.id
+  type              = "INGRESS"
+  protocol          = "TCP"
+  start_port        = 80
+  end_port          = 80
+  cidr              = "0.0.0.0/0"
+  description       = "Allow HTTP traffic"
+}
+
+resource "exoscale_sks_nodepool" "my_sks_nodepool" {
+  cluster_id         = exoscale_sks_cluster.my_sks_cluster.id
+  zone               = exoscale_sks_cluster.my_sks_cluster.zone
+  name               = "${var.exoscale_cluster_name}-nodepool"
+  instance_type      = "standard.large"
+  size               = 1
+
+  security_group_ids = [
+    exoscale_security_group.web_sg.id
+  ]
+}
+
+resource "exoscale_sks_kubeconfig" "my_sks_kubeconfig" {
+  cluster_id = exoscale_sks_cluster.my_sks_cluster.id
+  zone       = exoscale_sks_cluster.my_sks_cluster.zone
+
+  user   = "kubernetes-admin"
+  groups = ["system:masters"]
+}
+
+resource "local_sensitive_file" "kubeconfig" {
+    filename = "./kubeconfig"
+    content = exoscale_sks_kubeconfig.my_sks_kubeconfig.kubeconfig
+}
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+exoscale_key = "DEIN-EXOSCALE-API-KEY"
+exoscale_secret = "DEIN-EXOSCALE-API-SECRET"
+exoscale_cluster_name = "radius-cluster"
