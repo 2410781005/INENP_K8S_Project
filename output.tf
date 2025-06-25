c6ea2adb38d2dfbf86698e348c3602372224e7c9
2410781005 <2410781005@fh-burgenland.at>
2025-06-25 13:46:36 +0200

    changed loadbalancer name

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "nlb_ip_address" {
-  value = exoscale_nlb.web_nlb.ip_address
+  value = exoscale_nlb.oauth_lb.ip_address
   description = "Public IP address of the Exoscale Load Balancer"
 }
